dde8e1792280b233c4784b8517caf7281a39992a
Ubuntu <ubuntu@ip-172-31-0-147.us-west-2.compute.internal>
2016-11-27 02:14:44 +0000

    update/fix the script to generate training and validation samples

--- gen_train_validation_sets.py
+++ gen_train_validation_sets.py
@@ -8,9 +8,12 @@ import numpy as np
 
 labels = ["ALB", "BET", "DOL", "LAG", "NoF", "OTHER", "SHARK", "YFT"]
 
-data_dir = os.path.expanduser("~/scratch/full/train/") # directory containing all the data
-validation_dir = "validation"
-training_dir = "train"
+data_dir = os.path.expanduser("~/workspace/fisheries/datasets/train") # directory containing all the data
+validation_dir = os.path.expanduser("~/workspace/validation")
+training_dir = os.path.expanduser("~/workspace/train")
+#nvalidation_samples = 1 
+#ntrain_samples = 5
+
 
 ntotal = 0
 for l in labels:
@@ -19,16 +22,14 @@ for l in labels:
     nfiles = len(files)
     ntotal += nfiles
     print(nfiles, ntotal)
-    nvalidation_samples = 1 # int(nfiles*0.1)) # 10% of the data
-    ntrain_samples = 5 #nfiles - nvalidation_samples
-    train_samples = np.random.choice(files, ntrain_samples)
+    nvalidation_samples = int(nfiles*0.1) # 10% of the data
     validation_samples = np.random.choice(files, nvalidation_samples)
     train_dir = os.path.join(training_dir, l)    
     val_dir = os.path.join(validation_dir, l)
     os.mkdir(train_dir)    
     os.mkdir(val_dir)
-    for ts in train_samples:
-        shutil.copy(os.path.join(src_dir, ts), train_dir)
-    for vs in validation_samples:
-        shutil.copy(os.path.join(src_dir, vs), val_dir)
+    for f in files:
+        dest_dir = val_dir if f in validation_samples else train_dir
+        shutil.copy(os.path.join(src_dir, f), dest_dir)
+
         
