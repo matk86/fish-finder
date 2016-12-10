48ecc0f696afe9244b88f622c84780aaa98c91cf
Kent Liu <kentliu@gmail.com>
2016-12-10 14:32:58 -0800

    moved data related files

---
+++ datasets/constants.py
@@ -0,0 +1,6 @@
+import os
+
+labels = ["ALB", "BET", "DOL", "LAG", "NoF", "OTHER", "SHARK", "YFT"]
+data_dir = os.path.expanduser("~/workspace/fisheries/datasets/train") # directory containing all the data
+validation_dir = os.path.expanduser("~/workspace/validation")
+training_dir = os.path.expanduser("~/workspace/train")
