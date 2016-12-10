8cfd8e9a307fdeda6525cbeffbdfdd9c7977c6c9
Kent Liu <kentliu@gmail.com>
2016-12-10 15:17:43 -0800

    moved slim files here

---
+++ slim/scripts/eval_v4_on_fishes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Where the training checkpoint and logs will be saved to.
+TRAIN_DIR=/tmp/fishes/inception_v4
+
+# Where the dataset is saved to.
+DATASET_DIR=/home/ubuntu/workspace/data
+
+echo "Running evaluation on training data" 
+
+python eval_image_classifier.py \
+  --checkpoint_path=${TRAIN_DIR}/all \
+  --eval_dir=${TRAIN_DIR}/all \
+  --dataset_name=fishes \
+  --dataset_split_name=train \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v4
+
+echo "Running evaluation on validation data"
+
+python eval_image_classifier.py \
+  --checkpoint_path=${TRAIN_DIR}/all \
+  --eval_dir=${TRAIN_DIR}/all \
+  --dataset_name=fishes \
+  --dataset_split_name=validation \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v4
+
